5f036a964a008184e6e81b3b3ed74e696726ae1f
Splay28 <36258718+Splay28@users.noreply.github.com>
2025-10-21 11:27:05 +0900

    Add abstract method for touch_with_id in touch.py

--- minidevice/touch/touch.py
+++ minidevice/touch/touch.py
@@ -23,3 +23,16 @@ class Touch(ABC):
             points (list): [(x,y),(x,y),(x,y)] 坐标列表
             duration (int): 持续时间. Defaults to 300.
         """
+
+    @abstractmethod
+    def touch_with_id(self, touch_id: int, x: int, y: int, operation: str):
+        """
+        touch_with_id 指定id点触控
+        需要外部延迟管理
+
+        Args:
+            touch_id (int): 触控点id
+            x (int): 横坐标
+            y (int): 纵坐标
+            operation (str): "down","move","up"
+        """
